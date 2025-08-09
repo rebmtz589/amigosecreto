0c947059f44a442f5804d5930a85d1931aec5b71
rebmtz589 <reb.mtz.589@gmail.com>
2025-08-08 19:37:41 -0600

    Se implemento una funcion para agregar amigos.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 let amigos = []; 
+function agregarAmigo() {
+
+    let amigoEntrada = document.getElementById('amigo');
+    let nombre = amigoEntrada.value;
+
+    if (nombre.trim() === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+
+    amigoEntrada.value = '';
+}
