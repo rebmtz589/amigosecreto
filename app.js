1a5d0d2bf5d49ca92aa8e28f2b7a7a6852e9b22c
rebmtz589 <reb.mtz.589@gmail.com>
2025-08-08 19:44:35 -0600

    Se implemento una funcion para actualizar la lista de amigos.

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ function agregarAmigo() {
     }
 
     amigos.push(nombre);
-
     amigoEntrada.value = '';
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i];
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    }
 }
